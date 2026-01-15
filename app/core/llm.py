b343e3f1a41ec450572b618002306380e19e2559
uselessV <77937873+IiamV@users.noreply.github.com>
2026-01-15 11:14:41 +0700

    Added more options for core local LLM.

--- app/core/llm.py
+++ app/core/llm.py
@@ -1,1 +1,53 @@
 #  This file is for LLM abstraction layer
+import ollama
+from config import settings
+from openai import OpenAI
+import logger
+from typing import Sequence
+
+client = OpenAI()
+log = logger.logger
+
+
+def local_llm(prompt: str) -> str | None:
+    response = ollama.chat(model=str(settings.LOCAL_LLM_MODEL),
+                           messages=[
+                           {
+                               "role": "user",
+                               "content": prompt
+                           }
+                           ],
+                           tools=None,
+                           stream=False,
+                           think=False,
+                           format=None,
+                           options=None,
+                           keep_alive=None
+                           )
+
+    logger.logger.info(
+        "LLM response: %s", response.model_dump_json(indent=1),
+    )
+    if response is None:
+        raise ValueError("None Local LLM Response from Core LLM")
+
+    if response.message.content is not None:
+        return response.message.content
+
+
+# def api_llm(prompt: str) -> str:
+#     response = client.chat.completions.create(
+#         model='openai/gpt-oss-120b:free',
+#         messages=[
+#             {
+#                 "role": "user",
+#                 "content": prompt
+#             }
+#         ]
+#     )
+
+#     return f"LLM response for: {response}"
+
+
+# if __name__ == "__main__":
+#     local_llm("What is today's date?")
