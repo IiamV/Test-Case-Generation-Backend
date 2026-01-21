20f08e7a35b00954de89fbcecbe48b66ffba62e5
uselessV <77937873+IiamV@users.noreply.github.com>
2026-01-21 21:00:53 +0700

    [feat] Add System Response model schema.

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,1 +1,19 @@
 #  This file is for defining the models within the schemas
+
+from pydantic import BaseModel
+from typing import List
+
+
+class TestCase(BaseModel):
+    id: str
+    description: str
+    steps: List[str]
+    inputs: List[str]
+    preconditions: List[str]
+    expected_result: str
+    postconditions: List[str]
+
+
+class SystemResponse(BaseModel):
+    status: str
+    message: str
