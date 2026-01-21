1b97d53da5178a7f32af5c7d107f3709ec669fb4
uselessV <77937873+IiamV@users.noreply.github.com>
2026-01-21 21:03:20 +0700

    [refactor] Remove unused Testcase schema model.

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -4,16 +4,6 @@ from pydantic import BaseModel
 from typing import List
 
 
-class TestCase(BaseModel):
-    id: str
-    description: str
-    steps: List[str]
-    inputs: List[str]
-    preconditions: List[str]
-    expected_result: str
-    postconditions: List[str]
-
-
 class SystemResponse(BaseModel):
     status: str
     message: str
