365c49f0feb35198d9d1c9e54117267ab9b5197f
uselessV <77937873+IiamV@users.noreply.github.com>
2026-01-26 11:25:14 +0700

    [feat:core/database] Update the SQLAlchemy to be Async.

--- app/core/database.py
+++ app/core/database.py
@@ -6,33 +6,44 @@ from sqlalchemy.orm import (
     Session,
 )
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Integer, text
+from sqlalchemy import String, Integer, text, Engine
 from app.core.config import settings
-from sqlalchemy.ext.asyncio import AsyncEngine
-
-engine = create_engine(
-    url=str(settings.DATABASE_URL),
-    max_overflow=20,
-    pool_pre_ping=True,
-    pool_size=10
-)
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, async_sessionmaker, AsyncAttrs, AsyncSession
+from sqlalchemy_utils import database_exists, create_database
 
 
-class Base(DeclarativeBase):
+class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False,
-)
+async def get_engine() -> AsyncEngine:
+    # if not database_exists(settings.DATABASE_URL):
+    #     create_database(settings.DATABASE_URL)
+
+    engine = create_async_engine(
+        url=str(settings.DATABASE_URL),
+        # connect_args={"sslmode": "require"},
+        max_overflow=20,
+        pool_pre_ping=True,
+        pool_size=10
+    )
+
+    return engine
 
 
-def get_session() -> Session:
-    return SessionLocal()
+async def get_session() -> AsyncSession:
+    engine = await get_engine()
+
+    async_session = async_sessionmaker(
+        bind=engine,
+        autoflush=False
+    )()
+
+    return async_session
 
 
-async def database_healthcheck(engine: AsyncEngine) -> None:
+async def database_healthcheck() -> None:
+    engine = await get_engine()
+
     async with engine.connect() as connection:
         await connection.execute(text("SELECT 1"))
