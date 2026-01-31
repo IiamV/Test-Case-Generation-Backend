752bbc5482c85781199b4ff9983bcc93d0b01ebf
uselessV <77937873+IiamV@users.noreply.github.com>
2026-01-31 23:19:54 +0700

    [refactor:routes/auth] Rename the response model to the correct model.

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from app.services.auth import jira_login, jira_callback
 from app.services.jira import get_all_jira_projects
-from app.models.schemas import JiraLoginResponse
+from app.models.schemas import JiraAuthResponse
 from fastapi.responses import RedirectResponse, JSONResponse
 from fastapi import FastAPI, Request
 
@@ -10,7 +10,7 @@ router = APIRouter()
 
 @router.api_route(
     path="/jira-login",
-    response_model=JiraLoginResponse,
+    response_model=JiraAuthResponse,
     summary="Jira Login",
     description="Redirects to Jira for authorization",
     responses={200: {"description": "Services Healthy"}},
@@ -23,12 +23,12 @@ async def jira_auth_login(request: Request):
 
 @router.api_route(
     path="/jira-callback",
-    # response_model=JiraLoginResponse,
+    response_model=JiraAuthResponse,
     summary="Jira Callback",
     description="Redirects to Jira for authorization",
     responses={200: {"description": "Services Healthy"}},
     methods=["GET"],
-    response_class=JSONResponse,
+    response_class=RedirectResponse
 )
 async def jira_auth_callback(request: Request):
     return await jira_callback(request)
